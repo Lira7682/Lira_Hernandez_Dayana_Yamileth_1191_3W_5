e2de91e84900bbf49e25192530bea02f8c305e0e
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-27 22:52:11 -0600

    Add files via upload

---
+++ lira_hernandez_dayana_yamileth_1191_3W_5.py
@@ -0,0 +1,11 @@
+print (" ") #Espacio a agregar
+print ("Lira Hernandez Dayana Yamileth: programa muestra si el numero natural es par o impar")
+print (" ") #Espacio a agregar
+
+d = int(input("Ingresa un numero: ")) #Indica al usuario ingresar un numero 
+if d % 2 == 0:                        #Inicia las funciones
+    print ("El numero ",d, "es par")  #Print imprime la funcion a presentar
+else:                   
+    print ("El numero ",d, "es impar")#Print imprime la funcion a presentar
+
+print (" ") #Espacio a agregar
